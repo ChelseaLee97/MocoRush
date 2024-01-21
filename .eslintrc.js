48311350ae0743c673a91a9ab9eead9a5abe170b
therne <retail3210@gmail.com>
2024-01-21 21:40:22 +0900

    feat: Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,64 @@
+const gitignore = ['node_modules', '**/dist', 'generated'];
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint', 'import'],
+  ignorePatterns: [...gitignore, '*.test.ts', 'scripts/**', '*.js'],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-inferrable-types': [
+      'warn',
+      {
+        ignoreParameters: true,
+      },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-redundant-type-constituents': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+    '@typescript-eslint/restrict-template-expressions': 'off',
+    // 'no-console': 'warn',
+    'import/no-named-as-default': 'error',
+    'import/no-cycle': ['error', { maxDepth: 5, ignoreExternal: true }],
+    'import/no-unused-modules': 'error',
+    'import/no-deprecated': 'off',
+    'import/namespace': 'off',
+    'import/no-duplicates': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+    },
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+      node: {
+        paths: ['src'],
+      },
+    },
+  },
+};
