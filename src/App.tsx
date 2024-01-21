ec9ca17daa6e1ab71f53d00dfbdfc3e87775d86e
therne <retail3210@gmail.com>
2024-01-21 22:02:05 +0900

    feat: Apply React95

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Button, GlobalStyle, ThemeProvider } from '@react95/core';
+import '@react95/icons/icons.css';
+import '@react95/sans-serif';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <ThemeProvider>
+    <GlobalStyle />
+    <Button>Click me!</Button>
+  </ThemeProvider>
+);
 
 export default App;
